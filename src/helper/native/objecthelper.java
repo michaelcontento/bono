e927bd89a406d857383fb2c86c56627be3cb3f4b
Michael Contento <michaelcontento@gmail.com>
2013-03-20 16:14:11 +0100

    add android support for ObjectHelper

---
+++ src/helper/native/objecthelper.java
@@ -0,0 +1,11 @@
+class ObjectHelper
+{
+    public static int Compare(Object left, Object right)
+    {
+        if (left == right) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+}
