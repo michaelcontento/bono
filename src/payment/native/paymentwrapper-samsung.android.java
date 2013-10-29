dad0a7f6852a82c282cf4d9b0859b41512c5eba4
Michael Contento <michaelcontento@gmail.com>
2013-10-29 16:09:00 +0100

    dont cache Activity and SamsungIapHelper

--- src/payment/native/paymentwrapper-samsung.android.java
+++ src/payment/native/paymentwrapper-samsung.android.java
@@ -22,13 +22,11 @@ class PaymentWrapper extends ActivityDelegate implements OnInitIapListener, OnGe
 {
     private static final int iapMode = SamsungIapHelper.IAP_MODE_COMMERCIAL;
     /* private static final int iapMode = SamsungIapHelper.IAP_MODE_TEST_SUCCESS; */
-    private static MonkeyGame activity = MonkeyGame.activity;
 
     private static final String TAG  = PaymentWrapper.class.getName();
     private static final String ITEM_DB_KEY = "avalon.payment.samsung";
     private static final int ITEM_AMOUNT = 25;
     private int transactionCounter = 0;
-    private SamsungIapHelper helper = null;
     private String itemGroupId = null;
     private String pendingPurchaseItemId = null;
     private boolean isInitialized = false;
@@ -45,31 +43,31 @@ class PaymentWrapper extends ActivityDelegate implements OnInitIapListener, OnGe
         }
         isInitialized = true;
 
-        activity.AddActivityDelegate(this);
-        activity.runOnUiThread(new Runnable() {
+        getActivity().AddActivityDelegate(this);
+        getActivity().runOnUiThread(new Runnable() {
             @Override
             public void run()
             {
-                helper = SamsungIapHelper.getInstance(activity, iapMode);
+                SamsungIapHelper helper = SamsungIapHelper.getInstance(getActivity(), iapMode);
                 helper.setOnInitIapListener(PaymentWrapper.this);
                 helper.setOnGetItemListListener(PaymentWrapper.this);
                 helper.setOnGetInboxListListener(PaymentWrapper.this);
 
-                if (helper.isInstalledIapPackage(activity)) {
-                    if (helper.isValidIapPackage(activity)) {
-                        helper.showProgressDialog(activity);
-                        helper.startAccountActivity(activity);
+                if (helper.isInstalledIapPackage(getActivity())) {
+                    if (helper.isValidIapPackage(getActivity())) {
+                        helper.showProgressDialog(getActivity());
+                        helper.startAccountActivity(getActivity());
                     } else {
                         helper.showIapDialog(
-                            activity,
-                            helper.getValueString(activity, "title_iap"),
-                            helper.getValueString(activity, "msg_invalid_iap_package"),
+                            getActivity(),
+                            helper.getValueString(getActivity(), "title_iap"),
+                            helper.getValueString(getActivity(), "msg_invalid_iap_package"),
                             true,
                             null
                         );
                     }
                 } else {
-                    helper.installIapPackage(activity);
+                    helper.installIapPackage(getActivity());
                 }
             }
         });
@@ -105,11 +103,12 @@ class PaymentWrapper extends ActivityDelegate implements OnInitIapListener, OnGe
         if (resultCode == Activity.RESULT_OK) {
             bindIapService();
         } else if (resultCode == Activity.RESULT_CANCELED) {
+            SamsungIapHelper helper = SamsungIapHelper.getInstance(getActivity(), iapMode);
             helper.dismissProgressDialog();
             helper.showIapDialog(
-                activity,
-                helper.getValueString(activity, "title_samsungaccount_authentication"),
-                helper.getValueString(activity, "msg_authentication_has_been_cancelled"),
+                getActivity(),
+                helper.getValueString(getActivity(), "title_samsungaccount_authentication"),
+                helper.getValueString(getActivity(), "msg_authentication_has_been_cancelled"),
                 false,
                 null
             );
@@ -121,21 +120,23 @@ class PaymentWrapper extends ActivityDelegate implements OnInitIapListener, OnGe
      */
     public void bindIapService()
     {
+        SamsungIapHelper helper = SamsungIapHelper.getInstance(getActivity(), iapMode);
         helper.bindIapService(new SamsungIapHelper.OnIapBindListener() {
             @Override
             public void onBindIapFinished(int result)
             {
+                SamsungIapHelper helper = SamsungIapHelper.getInstance(getActivity(), iapMode);
                 if (pendingPurchaseItemId == null) {
                     helper.dismissProgressDialog();
                 }
 
                 if (result == SamsungIapHelper.IAP_RESPONSE_RESULT_OK) {
-                    helper.safeInitIap(activity);
+                    helper.safeInitIap(getActivity());
                 } else {
                     helper.showIapDialog(
-                        activity,
-                        helper.getValueString(activity, "title_iap"),
-                        helper.getValueString(activity, "msg_iap_service_bind_failed"),
+                        getActivity(),
+                        helper.getValueString(getActivity(), "title_iap"),
+                        helper.getValueString(getActivity(), "msg_iap_service_bind_failed"),
                         false,
                         null
                     );
@@ -152,8 +153,9 @@ class PaymentWrapper extends ActivityDelegate implements OnInitIapListener, OnGe
     @Override
     public void onSucceedInitIap()
     {
+        SamsungIapHelper helper = SamsungIapHelper.getInstance(getActivity(), iapMode);
         helper.safeGetItemList(
-            activity,
+            getActivity(),
             itemGroupId,
             1, 1 + ITEM_AMOUNT,
             SamsungIapHelper.ITEM_TYPE_ALL
@@ -171,8 +173,10 @@ class PaymentWrapper extends ActivityDelegate implements OnInitIapListener, OnGe
 
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd", Locale.getDefault());
         String today = sdf.format(new Date());
+
+        SamsungIapHelper helper = SamsungIapHelper.getInstance(getActivity(), iapMode);
         helper.safeGetItemInboxTask(
-            activity,
+            getActivity(),
             itemGroupId,
             1, 1 + ITEM_AMOUNT,
             "20131001",
@@ -216,10 +220,11 @@ class PaymentWrapper extends ActivityDelegate implements OnInitIapListener, OnGe
             return;
         }
 
-        helper.showProgressDialog(activity);
+        SamsungIapHelper helper = SamsungIapHelper.getInstance(getActivity(), iapMode);
+        helper.showProgressDialog(getActivity());
         ++transactionCounter;
         helper.startPurchase(
-            activity,
+            getActivity(),
             SamsungIapHelper.REQUEST_CODE_IS_IAP_PAYMENT,
             itemGroupId,
             itemId
@@ -241,6 +246,7 @@ class PaymentWrapper extends ActivityDelegate implements OnInitIapListener, OnGe
         int statusCode        = SamsungIapHelper.IAP_PAYMENT_IS_CANCELED;
         String errorString    = "";
         PurchaseVO purchaseVO = null;
+        SamsungIapHelper helper = SamsungIapHelper.getInstance(getActivity(), iapMode);
 
         if (extras != null) {
             thirdPartyName = extras.getString(SamsungIapHelper.KEY_NAME_THIRD_PARTY_NAME);
@@ -250,9 +256,9 @@ class PaymentWrapper extends ActivityDelegate implements OnInitIapListener, OnGe
         } else {
             helper.dismissProgressDialog();
             helper.showIapDialog(
-                activity,
-                helper.getValueString(activity, "title_iap"),
-                helper.getValueString(activity, "msg_payment_was_not_processed_successfully"),
+                getActivity(),
+                helper.getValueString(getActivity(), "title_iap"),
+                helper.getValueString(getActivity(), "msg_payment_was_not_processed_successfully"),
                 false,
                 null
             );
@@ -261,14 +267,14 @@ class PaymentWrapper extends ActivityDelegate implements OnInitIapListener, OnGe
         if (resultCode == Activity.RESULT_OK) {
             if (statusCode == SamsungIapHelper.IAP_ERROR_NONE) {
                 purchaseVO = new PurchaseVO(extras.getString(SamsungIapHelper.KEY_NAME_RESULT_OBJECT));
-                helper.verifyPurchaseResult(activity, purchaseVO);
+                helper.verifyPurchaseResult(getActivity(), purchaseVO);
                 itemDbAdd(itemId);
                 itemDbSave();
             } else {
                 helper.dismissProgressDialog();
                 helper.showIapDialog(
-                    activity,
-                    helper.getValueString(activity, "title_iap"),
+                    getActivity(),
+                    helper.getValueString(getActivity(), "title_iap"),
                     errorString,
                     false,
                     null
@@ -277,9 +283,9 @@ class PaymentWrapper extends ActivityDelegate implements OnInitIapListener, OnGe
         } else if (resultCode == Activity.RESULT_CANCELED) {
             helper.dismissProgressDialog();
             helper.showIapDialog(
-                activity,
-                helper.getValueString(activity, "title_iap"),
-                helper.getValueString(activity, "msg_payment_cancelled"),
+                getActivity(),
+                helper.getValueString(getActivity(), "title_iap"),
+                helper.getValueString(getActivity(), "msg_payment_cancelled"),
                 false,
                 null
             );
@@ -293,9 +299,7 @@ class PaymentWrapper extends ActivityDelegate implements OnInitIapListener, OnGe
      */
     protected void onDestroy()
     {
-        if (helper != null) {
-            helper.stopRunningTask();
-        }
+        SamsungIapHelper.getInstance(getActivity(), iapMode).stopRunningTask();
     }
 
     /**
@@ -358,14 +362,15 @@ class PaymentWrapper extends ActivityDelegate implements OnInitIapListener, OnGe
 
     private SharedPreferences getUserPreferences()
     {
-        return activity.getPreferences(Context.MODE_PRIVATE);
+        return getActivity().getPreferences(Context.MODE_PRIVATE);
     }
 
     private void showNoInternetDialog()
     {
-        AlertDialog.Builder alert = new AlertDialog.Builder(activity);
-        alert.setTitle(helper.getValueString(activity, "title_iap"));
-        alert.setMessage(helper.getValueString(activity, "msg_no_internet"));
+        SamsungIapHelper helper = SamsungIapHelper.getInstance(getActivity(), iapMode);
+        AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+        alert.setTitle(helper.getValueString(getActivity(), "title_iap"));
+        alert.setMessage(helper.getValueString(getActivity(), "msg_no_internet"));
         alert.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which)
@@ -376,11 +381,16 @@ class PaymentWrapper extends ActivityDelegate implements OnInitIapListener, OnGe
 
     private boolean isInternetConnected()
     {
-        ConnectivityManager cm = (ConnectivityManager) activity.getSystemService(Activity.CONNECTIVITY_SERVICE);
+        ConnectivityManager cm = (ConnectivityManager) getActivity().getSystemService(Activity.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
 
         boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
         return isConnected;
+    }
+
+    private MonkeyGame getActivity()
+    {
+        return MonkeyGame.activity;
     }
 
     /**
