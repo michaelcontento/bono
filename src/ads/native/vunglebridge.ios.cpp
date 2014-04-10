4dd0dbb2f266cc476b32192f7ee32eb8c9294e01
Michael Contento <michaelcontento@gmail.com>
2014-04-10 10:20:48 +0200

    pause and resume background music for vungle ads

--- src/ads/native/vunglebridge.ios.cpp
+++ src/ads/native/vunglebridge.ios.cpp
@@ -1,15 +1,30 @@
 #import <vunglepub/vunglepub.h>
 
+@class MyVungleDelegate;
+@interface MyVungleDelegate : NSObject<VGVunglePubDelegate>
+{
+}
+- (void)vungleMoviePlayed:(VGPlayData*)playData;
+@end
+@implementation MyVungleDelegate
+- (void)vungleMoviePlayed:(VGPlayData*)playData
+{
+    if (app && app->audio) app->audio->ResumeMusic();
+}
+@end
+
 class VungleBridge
 {
 public:
     void static StartWithPubAppID(String id)
     {
-        VGUserData*  data  = [VGUserData defaultUserData];
-        NSString*    appID = id.ToNSString();
+        VGUserData* data  = [VGUserData defaultUserData];
+        NSString* appID = id.ToNSString();
+        MyVungleDelegate* delegate = [MyVungleDelegate alloc];
 
         [VGVunglePub allowAutoRotate:YES];
         [VGVunglePub startWithPubAppID:appID userData:data];
+        [VGVunglePub setDelegate:delegate];
     }
 
     bool static AdIsAvailable()
@@ -21,6 +36,8 @@ public:
     {
         MonkeyAppDelegate* delegate = (MonkeyAppDelegate*) [[UIApplication sharedApplication] delegate];
         if (!delegate || !delegate.viewController) return;
+
+        if (app && app->audio) app->audio->PauseMusic();
         [VGVunglePub playModalAd:delegate.viewController animated:YES];
     }
 };
